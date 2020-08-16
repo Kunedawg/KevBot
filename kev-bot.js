21edefd181b521b555a1578440feb1a62825a29c
Kevin Kuney <Kevin.Kuney@gmail.com>
2020-08-16 14:05:48 -0500

    First time setup of discord bot on heroku.

---
+++ kev-bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login("***REMOVED***");
+
+client.on('ready', ()=>{
+    console.log("The bot is logged in!");
+});
+
+client.on('message', message => {
+    if(message.author.bot)
+        return;
+
+    if(message.content.toLowerCase() === 'hello')
+        message.channel.send("Hey!")
+});
