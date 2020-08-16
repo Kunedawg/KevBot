ada1c4779661dfb24fa25821f37ee96302718f74
Kevin Kuney <Kevin.Kuney@gmail.com>
2020-08-16 15:27:07 -0500

    Refined code to utilzie a dict to pair up commands and audio files. Also added prefix to config file.

--- kev-bot.js
+++ kev-bot.js
@@ -3,34 +3,56 @@ const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 
+var audio_path = "./audio/"
+var cmd_audio_dict = {
+    "imback"        : "trex.mp3",
+    "bonanno"       : "bonanno.mp3"
+};
+
 client.once('ready', () => {
 	console.log('kev-bot is ready and logged in!');
 });
 
 client.on('message', message => {
-	if (message.content === 'k!BoNanNo') {
-        var VC = message.member.voice.channel;
-        // console.log(VC);
-        if (!VC)
-           return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
-        VC.join()
-        .then(connection => {
-            const dispatcher = connection.play('./audio/bonanno.mp3');
-            dispatcher.on("finish", end => {VC.leave()});
-        })
-        .catch(console.error);
-    } else if (message.content === 'k!imback') {
-        var VC = message.member.voice.channel;
-        // console.log(VC);
-        if (!VC)
-           return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
-        VC.join()
-        .then(connection => {
-            const dispatcher = connection.play('./audio/trex.mp3');
-            dispatcher.on("finish", end => {VC.leave()});
-        })
-        .catch(console.error);
+    for(var key in cmd_audio_dict){
+        if (message.content.toLowerCase() === (prefix + key)) {
+            var VC = message.member.voice.channel;
+            if (!VC)
+               return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
+            VC.join()
+            .then(connection => {
+                const dispatcher = connection.play(audio_path + cmd_audio_dict[key]);
+                dispatcher.on("finish", end => {VC.leave()});
+            })
+            .catch(console.error);   
+        }
     }
+
+	// if (message.content === 'k!BoNanNo') {
+    //     var VC = message.member.voice.channel;
+    //     if (!VC)
+    //        return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
+    //     VC.join()
+    //     .then(connection => {
+    //         const dispatcher = connection.play('./audio/bonanno.mp3');
+    //         dispatcher.on("finish", end => {VC.leave()});
+    //     })
+    //     .catch(console.error);
+    // } else if (message.content === 'k!imback') {
+    //     var VC = message.member.voice.channel;
+    //     if (!VC)
+    //        return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
+    //     VC.join()
+    //     .then(connection => {
+    //         const dispatcher = connection.play('./audio/trex.mp3');
+    //         dispatcher.on("finish", end => {VC.leave()});
+    //     })
+    //     .catch(console.error);
+    // }
 });
 
 client.login(token);
+
+// function playAudio() {
+
+// }
