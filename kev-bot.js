9c7b34975a1476dcd33ee67412835c804c22b095
Kevin Kuney <Kevin.Kuney@gmail.com>
2020-08-16 15:10:58 -0500

    added config.json which stores the token. Also changed command to be k!command

--- kev-bot.js
+++ kev-bot.js
@@ -1,22 +1,5 @@
-// const Discord = require('discord.js');
-// const client = new Discord.Client();
-
-// client.login("NzQ0MDMxNDY3OTMxNDM1MTA5.XzdTTQ.jmPutro7dQ2HWT56B7Y6MLD7Sok");
-
-// client.on('ready', ()=>{
-//     console.log("The bot is logged in!");
-// });
-
-// client.on('message', message => {
-//     if(message.author.bot)
-//         return;
-
-//     if(message.content.toLowerCase() === 'hello')
-//         message.channel.send("Fuck off Evan!")
-// });
-
 const Discord = require('discord.js');
-//const { prefix, token } = require('./config.json');
+const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 
@@ -25,7 +8,7 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === '!BoNanNo') {
+	if (message.content === 'k!BoNanNo') {
         var VC = message.member.voice.channel;
         // console.log(VC);
         if (!VC)
@@ -36,7 +19,7 @@ client.on('message', message => {
             dispatcher.on("finish", end => {VC.leave()});
         })
         .catch(console.error);
-    } else if (message.content === '!imback') {
+    } else if (message.content === 'k!imback') {
         var VC = message.member.voice.channel;
         // console.log(VC);
         if (!VC)
@@ -50,4 +33,4 @@ client.on('message', message => {
     }
 });
 
-client.login("NzQ0MDMxNDY3OTMxNDM1MTA5.XzdTTQ.jmPutro7dQ2HWT56B7Y6MLD7Sok");
+client.login(token);
