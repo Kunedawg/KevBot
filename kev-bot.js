de54e3862f76cbb787e0a28ae4385e77979b95a1
Kevin Kuney <Kevin.Kuney@gmail.com>
2020-08-16 15:57:36 -0500

    Code cleanup and moving audio stuff to config.json.

--- kev-bot.js
+++ kev-bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const { prefix, token, audio_path, cmd_audio_dict } = require('./config.json');
 
 const client = new Discord.Client();
 
-var audio_path = "./audio/"
-var cmd_audio_dict = {
-    "imback"        : "trex.mp3",
-    "bonanno"       : "bonanno.mp3"
-};
+// var audio_path = "./audio/"
+// var cmd_audio_dict = {
+//     "imback"        : "trex.mp3",
+//     "bonanno"       : "bonanno.mp3"
+// };
 
 client.once('ready', () => {
 	console.log('kev-bot is ready and logged in!');
@@ -36,32 +36,6 @@ client.on('message', message => {
             .catch(console.error);   
         }
     }
-
-	// if (message.content === 'k!BoNanNo') {
-    //     var VC = message.member.voice.channel;
-    //     if (!VC)
-    //        return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
-    //     VC.join()
-    //     .then(connection => {
-    //         const dispatcher = connection.play('./audio/bonanno.mp3');
-    //         dispatcher.on("finish", end => {VC.leave()});
-    //     })
-    //     .catch(console.error);
-    // } else if (message.content === 'k!imback') {
-    //     var VC = message.member.voice.channel;
-    //     if (!VC)
-    //        return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
-    //     VC.join()
-    //     .then(connection => {
-    //         const dispatcher = connection.play('./audio/trex.mp3');
-    //         dispatcher.on("finish", end => {VC.leave()});
-    //     })
-    //     .catch(console.error);
-    // }
 });
 
 client.login(token);
-
-// function playAudio() {
-
-// }
