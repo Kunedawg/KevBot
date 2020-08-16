50657c7fbaf867629e26941dd964128d776578a0
Kevin Kuney <Kevin.Kuney@gmail.com>
2020-08-16 17:14:48 -0500

    New audio from mark. Also changed prefix from 'k!' to 'mark!'.

--- kev-bot.js
+++ kev-bot.js
@@ -8,25 +8,26 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
+    // Getting the user command
+    var user_command = message.content.toLowerCase();
     for(var key in cmd_audio_dict){
-        // Getting the user command and checking if it matches to dict
-        var user_command = message.content.toLowerCase();
+        // Generating dictionary command with prefix and key  
         dict_command = prefix + key;
         if (user_command === dict_command) {
             // Getting the voice channel that the member was in when the message was went
             var VC = message.member.voice.channel;
             // Verify voice channel is actually a voice channel
             if (!VC)
-               return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
-            // retrieve audio path from the dictionary
-            var audio_full_path = audio_path + cmd_audio_dict[key];  
+               return message.reply("YOU ARE NOT IN A VOICE CHANNEL")
+            // Retrieve audio path from the dictionary
+            var audio_full_path = audio_path + cmd_audio_dict[key]; 
             // Join channel, play mp3 from the dictionary, leave when completed.
             VC.join()
             .then(connection => {
                 const dispatcher = connection.play(audio_full_path);
                 dispatcher.on("finish", end => {VC.leave()});
             })
-            .catch(console.error);   
+            .catch(console.error);
         }
     }
 });
