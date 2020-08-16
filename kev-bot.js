a648eceb5c94ffca1e222d0f0926878941ae37e3
Kevin Kuney <Kevin.Kuney@gmail.com>
2020-08-16 16:07:04 -0500

    Further clean up of code.

--- kev-bot.js
+++ kev-bot.js
@@ -3,34 +3,27 @@ const { prefix, token, audio_path, cmd_audio_dict } = require('./config.json');
 
 const client = new Discord.Client();
 
-// var audio_path = "./audio/"
-// var cmd_audio_dict = {
-//     "imback"        : "trex.mp3",
-//     "bonanno"       : "bonanno.mp3"
-// };
-
 client.once('ready', () => {
 	console.log('kev-bot is ready and logged in!');
 });
 
 client.on('message', message => {
     for(var key in cmd_audio_dict){
+        // Getting the user command and checking if it matches to dict
         var user_command = message.content.toLowerCase();
-        console.log(user_command);
         dict_command = prefix + key;
-        console.log(dict_command);
         if (user_command === dict_command) {
-            console.log("Command match with key: " + key);
-            var full_path = audio_path + cmd_audio_dict[key];
+            // Getting the voice channel that the member was in when the message was went
             var VC = message.member.voice.channel;
+            // Verify voice channel is actually a voice channel
             if (!VC)
                return message.reply("MESSAGE IF NOT IN A VOICE CHANNEL")
+            // retrieve audio path from the dictionary
+            var audio_full_path = audio_path + cmd_audio_dict[key];  
+            // Join channel, play mp3 from the dictionary, leave when completed.
             VC.join()
             .then(connection => {
-                //var full_path = audio_path + cmd_audio_dict[key];
-                console.log("Key: "+ key + ". Value: " + cmd_audio_dict[key]);
-                console.log(full_path);
-                const dispatcher = connection.play(full_path);
+                const dispatcher = connection.play(audio_full_path);
                 dispatcher.on("finish", end => {VC.leave()});
             })
             .catch(console.error);   
