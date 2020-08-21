1d42eaeeba80abb76aa3675f2426571a7699d3e9
Kevin Kuney <Kevin.Kuney@gmail.com>
2020-08-20 22:33:40 -0500

    Restructed code to use a command handler. Added additional checks and created a banned_audio folder.

---
+++ commands/play.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+    name: 'p',
+    description: 'play an audio file by name',
+    args: true,
+    execute(message, args) {
+        // import the audio dict
+        const kev_bot = require('../kev-bot');
+
+        // Getting the user command
+        var file_to_play = args[0];
+        if (kev_bot.audio_dict.hasOwnProperty(file_to_play)) {
+            // Getting the voice channel that the member was in when the message was went
+            var VC = message.member.voice.channel;
+
+            // Verify voice channel is actually a voice channel
+            if (!VC) return message.reply("YOU ARE NOT IN A VOICE CHANNEL");
+
+            // Join channel, play mp3 from the dictionary, leave when completed.
+            VC.join()
+            .then(connection => {
+                const dispatcher = connection.play(kev_bot.audio_dict[file_to_play]);
+                dispatcher.on("finish", end => {VC.leave()});
+            })
+            .catch(console.error);
+        }
+    }
+};
