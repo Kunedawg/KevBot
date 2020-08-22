de98eddc4ce5624d6d418346cc20e00cb16c0e63
Kevin Kuney <Kevin.Kuney@gmail.com>
2020-08-21 22:42:18 -0500

    Added check to make sure args exists.

--- kev-bot.js
+++ kev-bot.js
@@ -53,7 +53,11 @@ client.on('message', message => {
     // Get args and commands name. Format of every command should follow "[prefix][command]!arg1 arg2 arg3 arg4"
     const prefix_removed = message.content.slice(prefix.length).trim().split('!'); // ["command", "arg1 arg2 arg3 arg4"]
     const commandName = prefix_removed[0]; // "command"
-    const args = prefix_removed[1].split(/ +/); // array of the args ["arg1", "arg2", "arg3", "arg4"]
+    if (typeof prefix_removed[1] === 'undefined') {
+        return;
+    } else {
+        const args = prefix_removed[1].split(/ +/); // array of the args ["arg1", "arg2", "arg3", "arg4"]  
+    }
 
     // Check that command exists
     if (!client.commands.has(commandName)) return;
