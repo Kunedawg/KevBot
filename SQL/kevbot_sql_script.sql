7cb0337c3b4424e204b5cb75cbbcbcacf0093bbf
Kevin Kuney <Kevin.Kuney@gmail.com>
2020-11-15 20:41:35 -0600

    latest sql schema

--- SQL/kevbot_sql_script.sql
+++ SQL/kevbot_sql_script.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: ***REMOVED***    Database: ***REMOVED***
 -- ------------------------------------------------------
@@ -7,7 +7,7 @@
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,14 +21,14 @@
 
 DROP TABLE IF EXISTS `player_greetings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `player_greetings` (
   `greeting_id` int(11) NOT NULL AUTO_INCREMENT,
   `greeting` varchar(100) NOT NULL,
   `player_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`greeting_id`),
   KEY `player_id_idx` (`player_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=121 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,12 +37,12 @@ CREATE TABLE `player_greetings` (
 
 DROP TABLE IF EXISTS `player_info`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `player_info` (
   `player_id` int(11) NOT NULL AUTO_INCREMENT,
   `discord_id` varchar(45) NOT NULL,
   PRIMARY KEY (`player_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=221 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=261 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +62,7 @@ CREATE TABLE `player_info` (
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = '' */ ;
 DELIMITER ;;
-CREATE DEFINER=`***REMOVED***`@`%` PROCEDURE `add_new_player`(IN discord_id VARCHAR(45))
+CREATE DEFINER=`***REMOVED***`@`%` PROCEDURE `add_new_player`(IN discord_id VARCHAR(45), OUT return_message VARCHAR(100))
 BEGIN
 	DECLARE player_id INT;
     
@@ -74,6 +74,16 @@ BEGIN
     IF player_id IS NULL THEN
 		INSERT INTO player_info (discord_id)
 		VALUES (discord_id);
+		SELECT player_info.player_id 
+		INTO player_id
+		FROM player_info
+		WHERE player_info.discord_id = discord_id;
+    END IF;
+    
+	IF player_id IS NOT NULL THEN
+		SET return_message = 'Success';
+	ELSE
+		SET return_message = 'Failed';
     END IF;
 END ;;
 DELIMITER ;
@@ -91,14 +101,31 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = '' */ ;
 DELIMITER ;;
-CREATE DEFINER=`***REMOVED***`@`%` PROCEDURE `del_greeting`(IN discord_id VARCHAR(45))
+CREATE DEFINER=`***REMOVED***`@`%` PROCEDURE `del_greeting`(IN discord_id VARCHAR(45), OUT return_message VARCHAR(100))
 BEGIN
+	DECLARE player_id INT;
+	
 	DELETE FROM player_greetings
     WHERE player_greetings.player_id = (
 		SELECT player_info.player_id 
 		FROM player_info
 		WHERE player_info.discord_id = discord_id  
     );
+    
+    SELECT player_greetings.player_id 
+    INTO player_id
+    FROM player_greetings
+    WHERE player_greetings.player_id = (
+		SELECT player_info.player_id 
+		FROM player_info
+		WHERE player_info.discord_id = discord_id  
+    );
+    
+    IF player_id IS NULL THEN
+		SET return_message = 'Success';
+	ELSE
+		SET return_message = 'Failed';
+	END IF;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -115,10 +142,8 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = '' */ ;
 DELIMITER ;;
-CREATE DEFINER=`***REMOVED***`@`%` PROCEDURE `get_greeting`(IN discord_id VARCHAR(45))
+CREATE DEFINER=`***REMOVED***`@`%` PROCEDURE `get_greeting`(IN discord_id VARCHAR(45), OUT greeting VARCHAR(100))
 BEGIN
-	DECLARE greeting VARCHAR(100);
-
 	SELECT player_greetings.greeting
 	INTO greeting
 	FROM player_greetings
@@ -127,8 +152,6 @@ BEGIN
         FROM player_info
         WHERE player_info.discord_id = discord_id
     );
-    
-    SELECT greeting;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -145,10 +168,11 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = '' */ ;
 DELIMITER ;;
-CREATE DEFINER=`***REMOVED***`@`%` PROCEDURE `set_greeting`(IN discord_id VARCHAR(45), IN greeting VARCHAR(100))
-BEGIN
+CREATE DEFINER=`***REMOVED***`@`%` PROCEDURE `set_greeting`(IN discord_id VARCHAR(45), IN greeting VARCHAR(100), OUT return_message VARCHAR(100))
+sp: BEGIN
 	DECLARE player_id INT;
     DECLARE greeting_id INT;
+    DECLARE return_greeting VARCHAR(100);
     
     SELECT player_info.player_id 
     INTO player_id
@@ -156,11 +180,16 @@ BEGIN
     WHERE player_info.discord_id = discord_id;
     
     IF player_id IS NULL THEN
-		CALL add_new_player(discord_id);
-		SELECT player_info.player_id 
-		INTO player_id
-		FROM player_info
-		WHERE player_info.discord_id = discord_id;
+		CALL add_new_player(discord_id, @return_message);
+        IF @return_message = 'Success' THEN
+			SELECT player_info.player_id 
+			INTO player_id
+			FROM player_info
+			WHERE player_info.discord_id = discord_id;
+		ELSE
+			SET return_message = 'Failed. Could not add new player';
+			LEAVE sp;
+		END IF;
     END IF;
     
 	SELECT player_greetings.greeting_id
@@ -177,6 +206,14 @@ BEGIN
 			INSERT INTO player_greetings (greeting, player_id)
 			VALUES (greeting, player_id);
         END IF;
+		CALL get_greeting(discord_id,return_greeting);
+        IF greeting = return_greeting THEN
+			SET return_message = 'Success';
+        ELSE
+			SET return_message = 'Failed. greeting did not set correctly.';
+        END IF;
+	ELSE
+		SET return_message = 'Failed. player_id does not exist.';
     END IF;
 END ;;
 DELIMITER ;
@@ -194,4 +231,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-14 19:37:03
+-- Dump completed on 2020-11-15 20:41:08
