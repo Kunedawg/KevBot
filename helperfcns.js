ba55c4b6f357aa5a18619fa2003de1b5a1c47294
Kevin Kuney <Kevin.Kuney@gmail.com>
2020-11-29 01:37:35 -0600

    cleaned up helperfcns and init

--- helperfcns.js
+++ helperfcns.js
@@ -2,80 +2,95 @@
 const {Storage, Bucket} = require('@google-cloud/storage');
 var ffmpeg = require('fluent-ffmpeg');
 
-module.exports = {
-    breakUpResponse(response, splitChar = '!@#', wrapChar = '```') {
-        const MAX_NO_WRAP_LENGTH = 2000 - 2 * wrapChar.length; // Largest message discord can send
+// function to break responses into 2000 char length (max discord allows)
+function breakUpResponse(response, splitChar = '!@#', wrapChar = '```') {
+    const MAX_NO_WRAP_LENGTH = 2000 - 2 * wrapChar.length; // Largest message discord can send
 
-        // Need to make sure every element in the array does not exceed MAX_NO_WRAP_LENGTH
-        // Perform splits on the splitChar, then newline, then string length if needed
-        subResponseArray = [];
-        for (let splitElement of response.split(splitChar)) {
-            if (splitElement.length <= MAX_NO_WRAP_LENGTH) {
-                subResponseArray.push(splitElement);
-            } else {
-                for (let newLineElement of splitElement.split('\n')) {
-                    if ((newLineElement + '\n').length <= MAX_NO_WRAP_LENGTH) {
-                        if (newLineElement.length > 0) subResponseArray.push(newLineElement + '\n');
-                    } else {
-                        const regex = new RegExp(`(.{${MAX_NO_WRAP_LENGTH}})`);
-                        for (let element of newLineElement.split(regex).filter(O=>O)) {
-                            subResponseArray.push(element);
-                        }
+    // Need to make sure every element in the array does not exceed MAX_NO_WRAP_LENGTH
+    // Perform splits on the splitChar, then newline, then string length if needed
+    subResponseArray = [];
+    for (let splitElement of response.split(splitChar)) {
+        if (splitElement.length <= MAX_NO_WRAP_LENGTH) {
+            subResponseArray.push(splitElement);
+        } else {
+            for (let newLineElement of splitElement.split('\n')) {
+                if ((newLineElement + '\n').length <= MAX_NO_WRAP_LENGTH) {
+                    if (newLineElement.length > 0) subResponseArray.push(newLineElement + '\n');
+                } else {
+                    const regex = new RegExp(`(.{${MAX_NO_WRAP_LENGTH}})`);
+                    for (let element of newLineElement.split(regex).filter(O=>O)) {
+                        subResponseArray.push(element);
                     }
                 }
             }
         }
-
-        // Merge strings back together into chuncks that do not exceed MAX_NO_WRAP_LENGTH
-        let strBuild = '';
-        let responseArray = [];
-        for (let subResponse of subResponseArray) {
-            if ((strBuild + subResponse).length > MAX_NO_WRAP_LENGTH) {
-                responseArray.push(wrapChar + strBuild + wrapChar);
-                strBuild = subResponse;
-            } else {
-                strBuild += subResponse;
-            }
-        }
-        if (strBuild.length > 0 && strBuild.length <= MAX_NO_WRAP_LENGTH) responseArray.push(wrapChar + strBuild + wrapChar);
-
-        return responseArray;
-    },
-    /**
-     * @param {Bucket} bucket
-     */
-    getFiles(bucket) {
-        return new Promise((resolve,reject) => {
-            bucket.getFiles((err,files)=>{
-                if (err) return reject(err);
-                let fileNameArray = [];
-                for (var file of files) fileNameArray.push(file.name);
-                return resolve(fileNameArray);
-            });
-        });
-    },
-    kevbotStringOkay(string){
-        const lowercaseOnly = /^[a-z\d]+$/g;
-        return lowercaseOnly.test(string);
-    },
-    normalizeAudio(inputPath,outputPath) {
-        return new Promise((resolve,reject) => {
-            ffmpeg(inputPath)
-                .audioFilters('loudnorm=I=-16:LRA=11:TP=-1.5')
-                .on('error', function(err) {
-                    return reject(err);
-                })
-                .on('end', function() {
-                    return resolve('Normalizing finished!');
-                })
-                .save(outputPath);
-        });
-    },
-    asyncPipe(reader,writer) {
-        return new Promise((resolve,reject) => {
-            reader.pipe(writer);
-            reader.on('end', () => {return resolve("pipe done!")});
-            setTimeout(()=>{return reject("pipe timed out after 20 sec!")}, 20000);
-        });        
     }
+
+    // Merge strings back together into chuncks that do not exceed MAX_NO_WRAP_LENGTH
+    let strBuild = '';
+    let responseArray = [];
+    for (let subResponse of subResponseArray) {
+        if ((strBuild + subResponse).length > MAX_NO_WRAP_LENGTH) {
+            responseArray.push(wrapChar + strBuild + wrapChar);
+            strBuild = subResponse;
+        } else {
+            strBuild += subResponse;
+        }
+    }
+    if (strBuild.length > 0 && strBuild.length <= MAX_NO_WRAP_LENGTH) responseArray.push(wrapChar + strBuild + wrapChar);
+
+    return responseArray;
+}
+
+// Gets files from a google cloud bucket
+/**
+ * @param {Bucket} bucket
+ */
+function getFiles(bucket) {
+    return new Promise((resolve,reject) => {
+        bucket.getFiles((err,files)=>{
+            if (err) return reject(err);
+            let fileNameArray = [];
+            for (var file of files) fileNameArray.push(file.name);
+            return resolve(fileNameArray);
+        });
+    });
+}
+
+// Tests if a string is lowercase and numbers only (for validating file names)
+function kevbotStringOkay(string){
+    const regex = /^[a-z\d]+$/g;
+    return regex.test(string);
+}
+
+// normalizes mp3 files
+function normalizeAudio(inputPath,outputPath) {
+    return new Promise((resolve,reject) => {
+        ffmpeg(inputPath)
+            .audioFilters('loudnorm=I=-16:LRA=11:TP=-1.5')
+            .on('error', function(err) {
+                return reject(err);
+            })
+            .on('end', function() {
+                return resolve('Normalizing finished!');
+            })
+            .save(outputPath);
+    });
+}
+
+// turns piping into an async operation
+function asyncPipe(reader,writer) {
+    return new Promise((resolve,reject) => {
+        reader.pipe(writer);
+        reader.on('end', () => {return resolve("pipe done!")});
+        setTimeout(()=>{return reject("pipe timed out after 20 sec!")}, 20000);
+    });        
+}
+
+module.exports = {
+    breakUpResponse,
+    getFiles,
+    kevbotStringOkay,
+    normalizeAudio,
+    asyncPipe
 }
--- init.js
+++ init.js
@@ -4,93 +4,98 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const hf = require('./helperfcns.js');
 const config = require('./config.json');
-const {Storage, Bucket} = require('@google-cloud/storage');
+const {Storage} = require('@google-cloud/storage');
 const path = require('path');
-const { getAudioDict } = require('./globaldata.js');
+
+// Creating dictionary for command to audio file path. yes -> ./audio/yes.mp3
+function Audio(download = true){
+    return new Promise(async(resolve,reject) => {
+        try {
+            // download all of the audio stored on the cloud server
+            if (download) {
+                const gc = new Storage({
+                    projectId: config.cloudCredentials.project_id,
+                    credentials: config.cloudCredentials
+                });
+                var audioBucket = gc.bucket(config.bucketName);
+                var files = await audioBucket.getFiles();
+                for (f of files[0]) {
+                    console.log("downloading: ", f.name);
+                    await f.download({destination: path.join(gd.audioPath, f.name)});
+                }
+            }
+
+            // Store all of the file paths in a dictionary
+            for(var f of fs.readdirSync(gd.audioPath)) {
+                if (f.split(".")[1] === "mp3")
+                    gd.pushAudioDict( f.split('.')[0] , path.join(gd.audioPath, f));
+            }
+
+            // Return promise
+            return resolve("Audio inited!");
+        } catch(err) {
+            return reject("Audio failed to init!" + err);
+        }
+    });
+}
+
+// Creating dictionary of arrays for the categories
+// catergories.csv has format of audio_file,category1,category2,category3,...
+function Categories(){
+    return new Promise(async(resolve,reject) => {
+        try {
+            gd.pushCategoryDict("all", []); // just an empty array
+            var catData = fs.readFileSync(gd.categoriesCsvPath,'utf8').split(' ').join(''); // read categories string and remove spaces.
+            if (gd.getEnv() === 'deploy')  // windows uses \r\n, linux uses \n, apple uses \r
+                var catRows = catData.split("\n"); 
+            if (gd.getEnv() === 'test')
+                var catRows = catData.split("\r\n"); 
+            for (const row of catRows) {
+                var categories = row.split(",");
+                var audioFile = categories.shift();  // first item of row is the audioFile, the rest will be categories
+                for (const category of categories) {
+                    let catergoryDict = gd.getCategoryDict();
+                    if (category in catergoryDict) {
+                        catergoryDict[category].push(audioFile);
+                        gd.setCategoryDict(catergoryDict);
+                    } else {
+                        gd.pushCategoryDict(category, [audioFile]);
+                    }
+                }
+            }
+            
+            // Return promise
+            return resolve("Categories inited!");
+        } catch (err) {
+            return reject("Categories failed to init!" + err);
+        }
+    });
+}
+
+// Storing the commands in a collection
+function Commands(){
+    return new Promise(async(resolve,reject) => {
+        try {
+            // Loop over the commands
+            var tempClient = gd.getClient();
+            tempClient.commands = new Discord.Collection();
+            const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                const command = require(`./commands/${file}`);
+                tempClient.commands.set(command.name, command);
+            }
+            gd.setClient(tempClient);
+
+            // Return promise
+            return resolve("Commands inited!");
+        } catch (err) {
+            return reject("Commands failed to init!" + err);
+        }
+    });
+}
 
 module.exports = {
-    // Creating dictionary for command to audio file path. yes -> ./audio/yes.mp3
-    Audio(download = true){
-        return new Promise(async(resolve,reject) => {
-            try {
-                // download all of the audio stored on the cloud server
-                if (download) {
-                    const gc = new Storage({
-                        projectId: config.cloudCredentials.project_id,
-                        credentials: config.cloudCredentials
-                    });
-                    var audioBucket = gc.bucket(config.bucketName);
-                    var files = await audioBucket.getFiles();
-                    for (f of files[0]) {
-                        console.log("downloading: ", f.name);
-                        await f.download({destination: path.join(gd.audioPath, f.name)});
-                    }
-                }
-
-                // Store all of the file paths in a dictionary
-                for(var f of fs.readdirSync(gd.audioPath)) {
-                    if (f.split(".")[1] === "mp3")
-                        gd.pushAudioDict( f.split('.')[0] , path.join(gd.audioPath, f));
-                }
-    
-                // Return promise
-                return resolve("Audio inited!");
-            } catch(err) {
-                return reject("Audio failed to init!" + err);
-            }
-        });
-    },
-    // Creating dictionary of arrays for the categories
-    // catergories.csv has format of audio_file,category1,category2,category3,...
-    Categories() {
-        return new Promise(async(resolve,reject) => {
-            try {
-                gd.pushCategoryDict("all", []); // just an empty array
-                var catData = fs.readFileSync(gd.categoriesCsvPath,'utf8').split(' ').join(''); // read categories string and remove spaces.
-                if (gd.getEnv() === 'deploy')  // windows uses \r\n, linux uses \n, apple uses \r
-                    var catRows = catData.split("\n"); 
-                if (gd.getEnv() === 'test')
-                    var catRows = catData.split("\r\n"); 
-                for (const row of catRows) {
-                    var categories = row.split(",");
-                    var audioFile = categories.shift();  // first item of row is the audioFile, the rest will be categories
-                    for (const category of categories) {
-                        let catergoryDict = gd.getCategoryDict();
-                        if (category in catergoryDict) {
-                            catergoryDict[category].push(audioFile);
-                            gd.setCategoryDict(catergoryDict);
-                        } else {
-                            gd.pushCategoryDict(category, [audioFile]);
-                        }
-                    }
-                }
-                
-                // Return promise
-                return resolve("Categories inited!");
-            } catch (err) {
-                return reject("Categories failed to init!" + err);
-            }
-        });
-    },
-    // Storing the commands in a collection
-    Commands(){
-        return new Promise(async(resolve,reject) => {
-            try {
-                // Loop over the commands
-                var tempClient = gd.getClient();
-                tempClient.commands = new Discord.Collection();
-                const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-                for (const file of commandFiles) {
-                    const command = require(`./commands/${file}`);
-                    tempClient.commands.set(command.name, command);
-                }
-                gd.setClient(tempClient);
-
-                // Return promise
-                return resolve("Commands inited!");
-            } catch (err) {
-                return reject("Commands failed to init!" + err);
-            }
-        });
-    }
+    Audio,
+    Categories,
+    Commands
 }
