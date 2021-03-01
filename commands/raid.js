2dc6548784eac59d7413f96166aa50238b7171dc
CWebster1122 <c.webster1122@utexas.edu>
2021-03-01 17:06:26 -0600

    basic 'raid' command now functionable

--- commands/raid.js
+++ commands/raid.js
@@ -19,14 +19,26 @@ module.exports = {
             let time = args?.[2];
             // Get voice channel number and time info and print in terminal. E.g. channel number: 1 \n time: 10:00pm
             /*var channel = message?.member?.voice?.channel;*/
-            let VC = message?.member?.voice?.channel;
-            //var ch_test = VC;
+            // let VCCurrent = message?.member?.voice?.channel;
+            let VC = message?.member?.guild?.channels?.cache.filter(channel => channel.type === 'voice'); //.channel.type;
+            // let VCSort = VC.sort(channel => channel.type === 'rawPosition');
+            let VCSort = VC.sort(function(a,b){return a.rawPosition-b.rawPosition});
+            let VCId = Array.from(VCSort.keys());
+            // if (channelNum['status'] === undefined || channelNum['status'] === null) return reject({userMess: "That is not a voice channel, ya dingus!"})
+            let i = channelNum-1;
+            let VCSelect = VCSort.get(VCId[i]);
+            if (VCSelect === undefined) return reject({userMess: "That is not a voice channel, ya dingus!"});
+            // if (i<0 || i>VCId.length || i['status'] === undefined) return reject({userMess: "That is not a voice channel, ya dingus!"});
+            // let VCIds = VC.map(channel => channel.id);
+            // var ch_test = VC;
             let t_test = '10:00pm';
-            console.log('channel number: ', channelNum, '\ntime: ', time);
-            
+            // console.log('channel number: ', channelNum, '\ntime: ', time);
+            // console.log('current channel: \n', VCCurrent, '\nselected channel: \n', VCSelect);
+            // console.log('channels: \n', VCSort, '\ntime: ', time);
+            // console.log('channels: \n', VCId, '\ntime: ', time);
             // Calling the play command
             try {
-                await gd.client.commands.get('p').execute({audio : audioToPlay, voiceChannel : VC});
+                await gd.client.commands.get('p').execute({audio : audioToPlay, voiceChannel : VCSelect});
             } catch (err) {
                 return reject(err);
             }
