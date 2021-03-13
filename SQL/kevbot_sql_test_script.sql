6ace76928e76273883e4c9e985c8dfa37cddeeea
Kevin Kuney <Kevin.Kuney@gmail.com>
2021-03-13 16:42:28 -0600

    updating user names happens once per day now

--- SQL/kevbot_sql_test_script.sql
+++ SQL/kevbot_sql_test_script.sql
@@ -1,21 +1,20 @@
 /*greeting calls*/
-CALL get_greeting('1124', @greeting); SELECT @greeting;
-CALL set_greeting('1124','rockbody', @mess); SELECT @mess;
-CALL del_greeting('1124',@mess); SELECT @mess;
+-- CALL get_greeting('1124', @greeting); SELECT @greeting;
+-- CALL set_greeting('1124','rockbody', @mess); SELECT @mess;
+-- CALL del_greeting('1124',@mess); SELECT @mess;
 SELECT * FROM player_greetings;
 
 /*player info calls*/
-CALL add_new_player('1124',@mess); SELECT @mess;
-CALL get_player_id('1124', @player_id, @mess); SELECT @player_id, @mess;
+-- CALL add_new_player('1124',@mess); SELECT @mess;
+-- CALL get_player_id('1124', @player_id, @mess); SELECT @player_id, @mess;
+-- CALL update_discord_user_name('1124', 'mario', @mess); SELECT @mess;
 SELECT * FROM player_info;
 
-SELECT * FROM player_info WHERE discord_id = 101190799005335552;
-
 /*audio calls*/
-CALL add_audio('1124', 'newaudio', '9', @mess); SELECT @mess;
-CALL add_audio('145361690328825857', 'identify', '8', @mess); SELECT @mess;
-CALL del_audio('kindfawn', @mess); SELECT @mess;
-CALL get_audio_id('kindfawn', @audio_id, @mess); SELECT @audio_id, @mess;
+-- CALL add_audio('1124', 'newaudio', '9', @mess); SELECT @mess;
+-- CALL add_audio('145361690328825857', 'identify', '8', @mess); SELECT @mess;
+-- CALL del_audio('kindfawn', @mess); SELECT @mess;
+-- CALL get_audio_id('kindfawn', @audio_id, @mess); SELECT @audio_id, @mess;
 SELECT * FROM audio;
 SELECT COUNT(*) FROM audio;
 SELECT * FROM audio WHERE player_id = 351;
@@ -33,21 +32,21 @@ INNER JOIN audio
 ON audio_play_log.audio_id = audio.audio_id;
 
 /*categories calls*/
-CALL add_category('1124', 'animals', @mess); SELECT @mess;
-CALL del_category('animals', @mess); SELECT @mess;
-CALL get_category_id('animals', @category_id, @mess); SELECT @category_id, @mess;
+-- CALL add_category('1124', 'animals', @mess); SELECT @mess;
+-- CALL del_category('animals', @mess); SELECT @mess;
+-- CALL get_category_id('animals', @category_id, @mess); SELECT @category_id, @mess;
 SELECT * FROM categories;
 SELECT discord_id FROM categories INNER JOIN player_info ON player_info.player_id = categories.player_id WHERE category_name = 'bingo';
 
 /*audio_category calls*/
-CALL add_audio_category('1124', 'kindfawn', 'animals', @mess); SELECT @mess;
-CALL del_audio_category('kindfawn', 'animals', @mess); SELECT @mess;
+-- CALL add_audio_category('1124', 'kindfawn', 'animals', @mess); SELECT @mess;
+-- CALL del_audio_category('kindfawn', 'animals', @mess); SELECT @mess;
 SELECT * FROM audio_category;
 
 /*audio play log calls*/
-CALL log_audio_play('1124', '4d3d', '0', @mess); SELECT @mess;
+-- CALL log_audio_play('1124', '4d3d', '0', @mess); SELECT @mess;
 SELECT * FROM audio_play_log;
 
 /*audio play log calls*/
-CALL log_category_play('1124', 'timeric', @mess); SELECT @mess;
+-- CALL log_category_play('1124', 'timeric', @mess); SELECT @mess;
 SELECT * FROM category_play_log;
--- kev-bot.js
+++ kev-bot.js
@@ -2,7 +2,9 @@
 const gd = require('./globaldata.js');
 const init = require('./init.js');
 const event = require('./eventhandlers.js');
-const {oncePerHour} = require("./functions/oncePerHour.js")
+const {oncePerHour} = require("./functions/oncePerHour.js");
+const {oncePerDay} = require("./functions/oncePerDay.js");
+const {updateUserNames} = require('./functions/updateUserNames.js');
 
 // Initialization
 async function initialize(){
@@ -11,6 +13,7 @@ async function initialize(){
     console.log(await init.categories());
     console.log(await init.commands());
     await gd.client.login(process.env.BOT_TOKEN);
+    await updateUserNames();
 }
 initialize().catch((err) => {
     console.error(err);
@@ -24,6 +27,7 @@ try {
     gd.client.on('voiceStateUpdate', (a,b) => {event.onVoiceStateUpdate(a,b)});
     gd.client.on('message', (message) => {event.onMessage(message,process.env.COMMAND_PREFIX)});
     setInterval(oncePerHour, 1000*60*60);  // once per hour updates
+    setInterval(oncePerDay, 1000*60*60*24);  // once per day updates
 } catch(err) {
     console.error(err);
 }
