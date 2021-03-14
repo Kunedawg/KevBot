3301e908cf327d2b15b363e62a8ea646ac2f30bc
Kevin Kuney <Kevin.Kuney@gmail.com>
2021-03-13 20:35:52 -0600

    Updated deleteDefiners to remove definers from data file as well

--- SQL/database_dumps/deleteDefiners.js
+++ SQL/database_dumps/deleteDefiners.js
@@ -4,21 +4,38 @@ const path = require('path');
 
 // Setup file paths
 const schema_path_raw = path.join(__dirname, './schema_raw.sql');
-const schema_path_raw_no_definers = path.join(__dirname, './schema_no_definers.sql');
+const schema_path_no_definers = path.join(__dirname, './schema_no_definers.sql');
+const data_path_raw = path.join(__dirname, './data_raw.sql');
+const data_path__no_definers = path.join(__dirname, './data_no_definers.sql');
 
 // Create write stream and line reader
-var writeStream = fs.createWriteStream(schema_path_raw_no_definers);
-var lineReader = require('readline').createInterface({input: require('fs').createReadStream(schema_path_raw)});
+var lineReaderSchema = require('readline').createInterface({input: require('fs').createReadStream(schema_path_raw)});
+var writeStreamSchema = fs.createWriteStream(schema_path_no_definers);
+var lineReaderData = require('readline').createInterface({input: require('fs').createReadStream(data_path_raw)});
+var writeStreamData = fs.createWriteStream(data_path__no_definers);
 
 // Write line to no_definers file with DEFINER removed
-lineReader.on('line', function (line) {
+lineReaderSchema.on('line', function (line) {
     if (line.includes("DEFINER")) {
         line = line.replace(/(DEFINER=`.*`@`%` )/g, "");
     }
-    writeStream.write(line + "\n", 'utf8');
+    writeStreamSchema.write(line + "\n", 'utf8');
 });
 
 // Close write steam on line reader finish
-lineReader.on('close', (input) => {
-    writeStream.end();
+lineReaderSchema.on('close', (input) => {
+    writeStreamSchema.end();
+});
+
+// Write line to no_definers file with DEFINER removed
+lineReaderData.on('line', function (line) {
+    if (line.includes("DEFINER")) {
+        line = line.replace(/(DEFINER=`.*`@`%` )/g, "");
+    }
+    writeStreamData.write(line + "\n", 'utf8');
+});
+
+// Close write steam on line reader finish
+lineReaderData.on('close', (input) => {
+    writeStreamData.end();
 });
