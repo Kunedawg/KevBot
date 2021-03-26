47fa67ec6501244889487651fd86b00f689c17d8
Kevin Kuney <Kevin.Kuney@gmail.com>
2021-03-25 23:05:59 -0500

    continued work on updates to the normalizer

--- test_code/ffmpeg_norm.sh
+++ test_code/ffmpeg_norm.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 ffmpeg -i $1 -af loudnorm=I=-16:LRA=11:TP=-1.5 "${1%.mp3}_norm.mp3"
--- test_code/ffmpeg_stats2.sh
+++ test_code/ffmpeg_stats2.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 ffmpeg -i $1 -af loudnorm=I=-16:TP=-1.5:LRA=11:print_format=json -f null -
