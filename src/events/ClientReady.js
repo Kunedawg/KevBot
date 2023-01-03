e95edc728a056668a192aae7b4721704165e3c9d
Kid_Kev <kevin.kuney@gmail.com>
2023-01-02 22:43:20 -0600

    event handlers working, I think

--- src/events/ClientReady.js
+++ src/events/ClientReady.js
@@ -1,5 +1,6 @@
 const { Events, ActivityType, Client } = require("discord.js");
-const AVATAR_PATH = path.join(__dirname, "../docs/pumping-iron-cropped.png");
+const path = require("path");
+const AVATAR_PATH = path.join(__dirname, "../images/pumping-iron-cropped.png");
 
 module.exports = {
   name: Events.ClientReady,
@@ -8,12 +9,8 @@ module.exports = {
    * @param {Client} client
    */
   async execute(client) {
-    try {
-      await client.user.setAvatar(AVATAR_PATH);
-      await client.user.setActivity("kev-bot | help!kb", { type: ActivityType.Playing });
-      console.log(`Logged in as ${client.user.tag}!`);
-    } catch (err) {
-      console.log(err);
-    }
+    await client.user.setAvatar(AVATAR_PATH);
+    await client.user.setActivity("kev-bot | help!kb", { type: ActivityType.Playing });
+    console.log(`Logged in as ${client.user.tag}!`);
   },
 };
--- src/events/OncePerHour.js
+++ src/events/OncePerHour.js
@@ -1,19 +1,11 @@
-const { updateMostPlayed } = require("../functions/updateMostPlayed.js");
-const { updateUploadsByUsers } = require("../functions/updateUploadsByUsers.js");
+const { updateMostPlayed } = require("../functions/updaters/updateMostPlayed.js");
+const { updateUploadsByUsers } = require("../functions/updaters/updateUploadsByUsers.js");
 
 module.exports = {
   name: "OncePerHour",
   once: false,
-  execute() {
-    return new Promise(async (resolve, reject) => {
-      try {
-        await updateMostPlayed();
-        await updateUploadsByUsers();
-      } catch (err) {
-        return reject(err);
-        // TODO: this reject is not going to get caught anywhere
-      }
-      return resolve();
-    });
+  async execute() {
+    await updateMostPlayed();
+    await updateUploadsByUsers();
   },
 };
