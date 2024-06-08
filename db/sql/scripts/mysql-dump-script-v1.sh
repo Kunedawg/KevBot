d294cf9b72abdf60eb20106f7b91fd095e30fb11
Kunedawg <Kevin.Kuney@gmail.com>
2024-06-08 16:23:11 -0500

    further updates to dump script

--- db/sql/scripts/mysql-dump-script-v1.sh
+++ db/sql/scripts/mysql-dump-script-v1.sh
@@ -13,7 +13,7 @@ check_missing_variables() {
 }
 
 # Specify the list of expected environment variables
-declare -a expected_env_variables=("SQL_DB_HOST" "SQL_DB_USER" "SQL_DB_PASSWORD" "SQL_DATABASE" "ENV")
+declare -a expected_env_variables=("SQL_DB_HOST" "SQL_DB_USER" "SQL_DB_PASSWORD" "SQL_DATABASE" "SQL_DUMP_DIR" "ENV")
 
 # Check for missing variables
 missing_variables=$(check_missing_variables "${expected_env_variables[@]}")
@@ -21,12 +21,12 @@ missing_variables=$(check_missing_variables "${expected_env_variables[@]}")
 # Check if there is a .env file when missing variables are not empty
 if [ -n "$missing_variables" ]; then
   # Check if the environment file is provided as an argument
-  if [ "$#" -lt 4 ]; then
+  if [ "$#" -ne 1 ]; then
     echo "There are missing env variables: ${missing_variables}"
-    echo "Usage: $0 <data_dump.sql> <schema_dump.sql> <full_dump.sql> [path/to/env_file]"
+    echo "Optionally provide env file. Usage: $0 <path/to/env_file>"
     exit 1
   fi
-  env_file="$4"
+  env_file="$1"
 
   # Load missing environment variables from .env file
   if [ -f "$env_file" ]; then
@@ -47,29 +47,37 @@ if [ -n "$missing_variables" ]; then
   exit 1
 fi
 
-# File names for the dumps
-DATA_FILE="$1"
-SCHEMA_FILE="$2"
-FULL_DUMP_FILE="$3"
-
 # Echo info
 echo "Dumping..."
 echo "HOST: $SQL_DB_HOST"
 echo "DATABASE: $SQL_DATABASE"
 
+# Generate vars
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+OUTPUT_DIR="$SQL_DUMP_DIR/$ENV"
+DATA_FILE="$OUTPUT_DIR/data_dump_$TIMESTAMP.sql"
+SCHEMA_FILE="$OUTPUT_DIR/schema_dump_$TIMESTAMP.sql"
+COMPLETE_DUMP_FILE="$OUTPUT_DIR/complete_dump_$TIMESTAMP.sql"
+
+# Check if the output directory exists, create it if not
+if [ ! -d "$OUTPUT_DIR" ]; then
+  echo "Making directory $OUTPUT_DIR"
+  mkdir -p "$OUTPUT_DIR"
+fi
+
 # Data Dump
 mysqldump -h "$SQL_DB_HOST" -u "$SQL_DB_USER" -p"$SQL_DB_PASSWORD" \
   --no-create-info --extended-insert "$SQL_DATABASE" >"$DATA_FILE"
-echo "Data dump completed: $DATA_FILE"
+echo "Data dump here: $DATA_FILE"
 
 # Schema Dump
 mysqldump -h "$SQL_DB_HOST" -u "$SQL_DB_USER" -p"$SQL_DB_PASSWORD" \
   --no-data --routines --skip-triggers "$SQL_DATABASE" >"$SCHEMA_FILE"
-echo "Schema dump completed: $SCHEMA_FILE"
+echo "Schema dump here: $SCHEMA_FILE"
 
 # Complete Dump
 mysqldump -h "$SQL_DB_HOST" -u "$SQL_DB_USER" -p"$SQL_DB_PASSWORD" \
-  --routines --triggers "$SQL_DATABASE" >"$FULL_DUMP_FILE"
-echo "Complete dump completed: $FULL_DUMP_FILE"
+  --routines --triggers "$SQL_DATABASE" >"$COMPLETE_DUMP_FILE"
+echo "Complete dump here: $COMPLETE_DUMP_FILE"
 
 echo "Dump completed!"
