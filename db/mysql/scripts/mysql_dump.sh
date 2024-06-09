1ed2932d68862b8eec912d55733f7f03a1b32066
Kunedawg <Kevin.Kuney@gmail.com>
2024-06-08 23:44:26 -0500

    restored mysql_dump.sh

--- db/mysql/scripts/mysql_dump.sh
+++ db/mysql/scripts/mysql_dump.sh
@@ -1,73 +1,87 @@
 #!/bin/bash
 
-# Function to load environment variables from a file
-load_env_file() {
-  local env_file=$1
-  if [ -f "$env_file" ]; then
-    set -o allexport
-    source "$env_file"
-    set -o allexport
-  else
-    echo "Environment file $env_file does not exist."
-    exit 1
-  fi
+# Function to check and report missing variables
+check_missing_variables() {
+  local variables_to_check=("$@")
+  local missing_variables=()
+  for var_name in "${variables_to_check[@]}"; do
+    if [ -z "${!var_name}" ]; then
+      missing_variables+=("$var_name")
+    fi
+  done
+  echo "${missing_variables[@]}"
 }
 
-# Check if the local directory argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <local-directory> [env-file]"
+# Specify the list of expected environment variables
+declare -a expected_env_variables=("SQL_DB_HOST" "SQL_DB_USER" "SQL_DB_PASSWORD" "SQL_DB_DATABASE" "SQL_DB_PORT" "SQL_DB_SSL_CA" "ENV")
+
+# Check for missing variables
+missing_variables=$(check_missing_variables "${expected_env_variables[@]}")
+
+# Check if there is a .env file when missing variables are not empty
+if [ -n "$missing_variables" ]; then
+  # Check if the environment file is provided as an argument
+  if [ "$#" -lt 4 ]; then
+    echo "There are missing env variables: ${missing_variables}"
+    echo "Usage: $0 <data_dump.sql> <schema_dump.sql> <full_dump.sql> [path/to/env_file]"
+    exit 1
+  fi
+  env_file="$4"
+
+  # Load missing environment variables from .env file
+  if [ -f "$env_file" ]; then
+    set -a
+    source "$env_file"
+    set -e
+  else
+    echo "Provided .env file does not exist: $env_file"
+    exit 1
+  fi
+fi
+
+# Recheck for missing variables after sourcing .env file
+missing_variables=$(check_missing_variables "${expected_env_variables[@]}")
+if [ -n "$missing_variables" ]; then
+  echo "Env vars still missing!!"
+  echo "missing vars: ${missing_variables}"
   exit 1
 fi
 
-# Assign the local directory from the argument
-LOCAL_DIRECTORY=$1
+# File names for the dumps
+DATA_FILE="$1"
+SCHEMA_FILE="$2"
+FULL_DUMP_FILE="$3"
 
-# Optional second argument for environment file
-ENV_FILE=$2
+# Echo info
+echo "Dumping..."
+echo "DATABASE: $SQL_DB_DATABASE"
 
-# Load environment variables from file if provided
-if [ -n "$ENV_FILE" ]; then
-  load_env_file "$ENV_FILE"
-fi
+# Write SSL CA string to a temporary file
+SSL_CA_FILE=$(mktemp)
+echo -e "$SQL_DB_SSL_CA" >"$SSL_CA_FILE"
 
-# Check if the required environment variables are set
-if [ -z "$GOOGLE_CLOUD_BUCKET_NAME" ]; then
-  echo "GOOGLE_CLOUD_BUCKET_NAME environment variable is not set."
-  exit 1
-fi
+# Set the MYSQL_PWD environment variable to suppress the password warning
+export MYSQL_PWD="$SQL_DB_PASSWORD"
 
-if [ -z "$GOOGLE_CLOUD_CREDENTIALS" ]; then
-  echo "GOOGLE_CLOUD_CREDENTIALS environment variable is not set."
-  exit 1
-fi
+# Data Dump
+mysqldump -h "$SQL_DB_HOST" -P "$SQL_DB_PORT" --ssl-ca="$SSL_CA_FILE" -u "$SQL_DB_USER" \
+  --single-transaction --set-gtid-purged=OFF --no-create-info --extended-insert "$SQL_DB_DATABASE" >"$DATA_FILE"
+echo "Data dump completed: $DATA_FILE"
 
-# Write the GOOGLE_CLOUD_CREDENTIALS JSON string to a temporary file
-echo "$GOOGLE_CLOUD_CREDENTIALS" >/tmp/google_credentials.json
+# Schema Dump
+mysqldump -h "$SQL_DB_HOST" -P "$SQL_DB_PORT" --ssl-ca="$SSL_CA_FILE" -u "$SQL_DB_USER" \
+  --no-data --routines --skip-triggers --set-gtid-purged=OFF "$SQL_DB_DATABASE" >"$SCHEMA_FILE"
+echo "Schema dump completed: $SCHEMA_FILE"
 
-# Authenticate with gcloud using the service account key
-gcloud auth activate-service-account --key-file=/tmp/google_credentials.json
+# Full Dump
+mysqldump -h "$SQL_DB_HOST" -P "$SQL_DB_PORT" --ssl-ca="$SSL_CA_FILE" -u "$SQL_DB_USER" \
+  --single-transaction --routines --triggers --set-gtid-purged=OFF "$SQL_DB_DATABASE" >"$FULL_DUMP_FILE"
+echo "Full dump completed: $FULL_DUMP_FILE"
 
-# Check if authentication was successful
-if [ $? -ne 0 ]; then
-  echo "Failed to authenticate with gcloud."
-  rm /tmp/google_credentials.json
-  exit 1
-fi
+# Unset the MYSQL_PWD environment variable after use
+unset MYSQL_PWD
 
-# Create the local directory if it doesn't exist
-mkdir -p "$LOCAL_DIRECTORY"
+# Clean up the temporary SSL CA file
+rm "$SSL_CA_FILE"
 
-# Download all files from the bucket to the local directory using gsutil
-gsutil -m cp -r gs://$GOOGLE_CLOUD_BUCKET_NAME/* $LOCAL_DIRECTORY
-
-# Verify if the download was successful
-if [ $? -eq 0 ]; then
-  echo "All files have been successfully downloaded from gs://$GOOGLE_CLOUD_BUCKET_NAME to $LOCAL_DIRECTORY."
-else
-  echo "An error occurred while downloading the files."
-  rm /tmp/google_credentials.json
-  exit 1
-fi
-
-# Clean up the temporary credentials file
-rm /tmp/google_credentials.json
+echo "Dump completed!"
