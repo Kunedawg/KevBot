b1847edaef5b906274ee49ebc8c8c832d12faae7
Kunedawg <Kevin.Kuney@gmail.com>
2024-06-09 21:22:24 -0500

    db_dump.sh is fully functional and dumps mysql and gcloud

--- db/README.md
+++ db/README.md
@@ -2,11 +2,17 @@
 
 ## Backup Scripts
 
-A shell script has been created for quickly taking backups. You can call it be providing an env file.
+A shell script has been created for quickly taking backups. You can call it by providing an env file. Note it should be called from the `db` directory.
 
 ```sh
 ./scripts/db_dump.sh prod.env
 ```
+
+- `./scripts/db_dump.sh` creates dumps of both mysql and glcoud.
+- `./mysql/scripts/mysql_dump.sh` dumps just mysql.
+- `./gcloud/scripts/gcloud_dump.sh` dumps just gcloud.
+
+### Requirements
 
 `mysqldump` is needed which can installed with homebrew.
 
--- db/scripts/db_dump.sh
+++ db/scripts/db_dump.sh
@@ -3,13 +3,15 @@
 # Function to display usage
 usage() {
     echo "Backs up all data from databases and other sources"
-    echo "Usage: $0 [path/to/env_file] [dump_folder] [mysql_dump_script]"
-    echo "  path/to/env_file  Optional: Path to the environment file."
-    echo "  dump_folder       Optional: Path to the dump folder. Defaults to ./dumps/ENV/TIMESTAMP_dump/"
-    echo "  mysql_dump_script Optional: Path to the mysql-dump script. Defaults to ./sql/scripts/mysql_dump.sh"
+    echo "Usage: $0 [path/to/env_file] [dump_folder] [mysql_dump_script] [gcloud_dump_script]"
+    echo "  path/to/env_file      Optional: Path to the environment file."
+    echo "  dump_folder           Optional: Path to the dump folder. Defaults to ./dumps/ENV/TIMESTAMP_dump/"
+    echo "  mysql_dump_script     Optional: Path to the mysql-dump script. Defaults to ./mysql/scripts/mysql_dump.sh"
+    echo "  gcloud_dump_script    Optional: Path to the gcloud-dump script. Defaults to ./gcloud/scripts/gcloud_dump.sh"
     echo "Environment Variables:"
-    echo "  SQL_DB_DATABASE   Required: Name of the database to dump."
-    echo "  ENV               Required: Environment name to include in the dump folder path."
+    echo "  SQL_DB_DATABASE           Required: Name of the database to dump."
+    echo "  GOOGLE_CLOUD_BUCKET_NAME  Required: Name of the Google Cloud bucket."
+    echo "  ENV                       Required: Environment name to include in the dump folder path."
     exit 0
 }
 
@@ -31,9 +33,14 @@ if [ -n "$ENV_FILE" ]; then
     fi
 fi
 
-# Check if SQL_DB_DATABASE and ENV are set after reading the env file
+# Check if SQL_DB_DATABASE, GOOGLE_CLOUD_BUCKET_NAME, and ENV are set after reading the env file
 if [ -z "$SQL_DB_DATABASE" ]; then
     echo "Error: SQL_DB_DATABASE environment variable is not set."
+    usage
+fi
+
+if [ -z "$GOOGLE_CLOUD_BUCKET_NAME" ]; then
+    echo "Error: GOOGLE_CLOUD_BUCKET_NAME environment variable is not set."
     usage
 fi
 
@@ -48,6 +55,7 @@ TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 # Arguments
 DUMP_FOLDER=${2:-./dumps/${ENV}/${TIMESTAMP}_dump/}
 MYSQL_DUMP_SCRIPT=${3:-./mysql/scripts/mysql_dump.sh}
+GCLOUD_DUMP_SCRIPT=${4:-./gcloud/scripts/gcloud_dump.sh}
 
 # Create dump folder if it doesn't exist
 mkdir -p "$DUMP_FOLDER"
@@ -56,6 +64,7 @@ mkdir -p "$DUMP_FOLDER"
 DATA_DUMP="${DUMP_FOLDER}${TIMESTAMP}_${SQL_DB_DATABASE}_data_dump.sql"
 SCHEMA_DUMP="${DUMP_FOLDER}${TIMESTAMP}_${SQL_DB_DATABASE}_schema_dump.sql"
 FULL_DUMP="${DUMP_FOLDER}${TIMESTAMP}_${SQL_DB_DATABASE}_full_dump.sql"
+GCLOUD_DUMP="${DUMP_FOLDER}${TIMESTAMP}_${GOOGLE_CLOUD_BUCKET_NAME}_gcloud_dump.zip"
 
 # Call the mysql-dump.sh script
 if [ -z "$ENV_FILE" ]; then
@@ -67,6 +76,19 @@ fi
 # Check the exit status of mysql-dump.sh
 if [ $? -ne 0 ]; then
     echo "Error: ${MYSQL_DUMP_SCRIPT} failed."
+    exit 1
+fi
+
+# Call the gcloud-dump.sh script
+if [ -z "$ENV_FILE" ]; then
+    "$GCLOUD_DUMP_SCRIPT" "$GCLOUD_DUMP"
+else
+    "$GCLOUD_DUMP_SCRIPT" "$GCLOUD_DUMP" "$ENV_FILE"
+fi
+
+# Check the exit status of gcloud-dump.sh
+if [ $? -ne 0 ]; then
+    echo "Error: ${GCLOUD_DUMP_SCRIPT} failed."
     exit 1
 fi
 
