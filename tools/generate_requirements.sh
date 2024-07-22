ec6c265bbe3e816860a4fc7053b493bc11a6a2a9
Kunedawg <Kevin.Kuney@gmail.com>
2024-07-22 17:44:49 -0600

    refactor(tools): Renamed mysql_manager
    - Renamed mysql_manager to migration_manager. Updated other code as
      needed.

--- tools/generate_requirements.sh
+++ tools/generate_requirements.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Navigate to the tool1 directory and export its dependencies
-cd db/mysql_manager
+cd db/migration_manager
 poetry export -f requirements.txt --output /tmp/requirements_tool1.txt
+cd ../../
 
 # Navigate to the tool2 directory and export its dependencies
 cd ../rename_to_id
 poetry export -f requirements.txt --output /tmp/requirements_tool2.txt
-
 cd ../../
+
 # Combine the two requirements files
 cat /tmp/requirements_tool1.txt /tmp/requirements_tool2.txt >/tmp/requirements.txt
 
