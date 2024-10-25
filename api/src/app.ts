4e8774d8b159014ad79c800dcb5590d11ab30c2e
Kunedawg <Kevin.Kuney@gmail.com>
2024-10-25 08:18:13 -1000

    feat(api): convert api docs to typescript

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,42 +1,31 @@
-// const express = require("express");
 import express, { Request, Response } from "express";
-// const cors = require("cors");
-// const morgan = require("morgan");
-const docRoutes = require("./routes/docRoutes");
+import docRoutes from "./routes/docRoutes";
 const tracksRoutes = require("./routes/tracksRoutes");
 const authRoutes = require("./routes/authRoutes");
 const usersRoutes = require("./routes/usersRoutes");
 const playlistsRoutes = require("./routes/playlistsRoutes");
 const playsRoutes = require("./routes/playsRoutes");
 const errorHandler = require("./middlewares/errorHandler");
-// const auth = require("./middlewares/auth");
-// import { requireAuth, AuthenticatedRequest } from "./middlewares/auth";
 import { requireAuth } from "./middlewares/auth";
 const initTaskSchedules = require("./schedulers/taskScheduler");
 
 const app = express();
 
-// app.use(cors());
 app.use(express.json());
-// app.use(morgan("dev")); // Logging middleware
 
-// Routes
 app.use("/v1/tracks", tracksRoutes);
 app.use("/v1/docs", docRoutes);
 app.use("/v1/auth", authRoutes);
 app.use("/v1/users", usersRoutes);
 app.use("/v1/playlists", playlistsRoutes);
 app.use("/v1/plays", playsRoutes);
-// testing
+// testing - remove
 app.get("/v1/protected", requireAuth, (req: Request, res: Response) => {
   res.send(`Hello ${req.user?.username}`);
 });
 
-// Global Error Handler
 app.use(errorHandler);
 
 initTaskSchedules();
-
-// module.exports = app;
 
 export default app;
--- api/src/routes/docRoutes.ts
+++ api/src/routes/docRoutes.ts
@@ -1,12 +1,10 @@
-const express = require("express");
-const YAML = require("yamljs");
-const path = require("path");
-const swaggerUi = require("swagger-ui-express");
+import express from "express";
+import YAML from "yamljs";
+import path from "path";
+import swaggerUi from "swagger-ui-express";
 
 const router = express.Router();
-
 const yamlFilePath = path.join(__dirname, "..", "docs", "kevbot-api.yml");
 const swaggerDocument = YAML.load(yamlFilePath);
 router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-module.exports = router;
+export default router;
