76a91ae5fd00e5a0450f519e8935c63234d685f8
Kunedawg <Kevin.Kuney@gmail.com>
2024-10-31 20:39:49 -0500

    refactor(api): use auth.requireAuth instead
    of requireAuth

--- api/src/routes/tracksRoutes.ts
+++ api/src/routes/tracksRoutes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import tracksController from "../controllers/tracksController";
 import upload from "../middlewares/multer";
-import { requireAuth } from "../middlewares/auth";
+import auth from "../middlewares/auth";
 
 const router = express.Router();
 
@@ -9,9 +9,9 @@ router.get("/", tracksController.getTracks);
 router.get("/:id", tracksController.getTrackById);
 router.get("/:id/download", tracksController.getTrackDownloadById);
 router.get("/:id/stream", tracksController.getTrackStreamById);
-router.patch("/:id", requireAuth, tracksController.patchTrack);
-router.post("/", requireAuth, upload.single("file"), tracksController.postTrack);
-router.delete("/:id", requireAuth, tracksController.deleteTrack);
-router.patch("/:id/restore", requireAuth, tracksController.restoreTrack);
+router.patch("/:id", auth.requireAuth, tracksController.patchTrack);
+router.post("/", auth.requireAuth, upload.single("file"), tracksController.postTrack);
+router.delete("/:id", auth.requireAuth, tracksController.deleteTrack);
+router.patch("/:id/restore", auth.requireAuth, tracksController.restoreTrack);
 
 export default router;
--- api/src/routes/usersRoutes.ts
+++ api/src/routes/usersRoutes.ts
@@ -1,22 +1,22 @@
 import express from "express";
 import usersController from "../controllers/usersController";
-import { requireAuth } from "../middlewares/auth";
+import auth from "../middlewares/auth";
 import setIdFromUser from "../middlewares/setIdFromUser";
 
 const router = express.Router();
 
 router.get("/", usersController.getUsers);
-router.get("/@me", requireAuth, setIdFromUser, usersController.getUserById);
-router.get("/@me/greeting", requireAuth, setIdFromUser, usersController.getGreeting);
-router.get("/@me/farewell", requireAuth, setIdFromUser, usersController.getFarewell);
+router.get("/@me", auth.requireAuth, setIdFromUser, usersController.getUserById);
+router.get("/@me/greeting", auth.requireAuth, setIdFromUser, usersController.getGreeting);
+router.get("/@me/farewell", auth.requireAuth, setIdFromUser, usersController.getFarewell);
 router.get("/:id", usersController.getUserById);
 router.get("/:id/greeting", usersController.getGreeting);
 router.get("/:id/farewell", usersController.getFarewell);
-router.patch("/@me", requireAuth, setIdFromUser, usersController.patchUser);
-router.patch("/:id", requireAuth, usersController.patchUser);
-router.put("/@me/greeting", requireAuth, setIdFromUser, usersController.putGreeting);
-router.put("/@me/farewell", requireAuth, setIdFromUser, usersController.putFarewell);
-router.put("/:id/greeting", requireAuth, usersController.putGreeting);
-router.put("/:id/farewell", requireAuth, usersController.putFarewell);
+router.patch("/@me", auth.requireAuth, setIdFromUser, usersController.patchUser);
+router.patch("/:id", auth.requireAuth, usersController.patchUser);
+router.put("/@me/greeting", auth.requireAuth, setIdFromUser, usersController.putGreeting);
+router.put("/@me/farewell", auth.requireAuth, setIdFromUser, usersController.putFarewell);
+router.put("/:id/greeting", auth.requireAuth, usersController.putGreeting);
+router.put("/:id/farewell", auth.requireAuth, usersController.putFarewell);
 
 export default router;
