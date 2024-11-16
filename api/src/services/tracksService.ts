0d92347b126460ad6360358a5aaebd6842e1bac0
Kunedawg <Kevin.Kuney@gmail.com>
2024-11-16 14:07:54 -0600

    refactor(api): refactor tracks handling
    - Refactored tracksController. Moved logic into the service layer.
    - Refactored playlistsService. Handles more of the business layer logic
      including permission checking, uniqueness, and existence checks.
    - Add tracksSchemas. Request schemas now exist here.

--- api/src/services/tracksService.ts
+++ api/src/services/tracksService.ts
@@ -3,6 +3,7 @@ import tracksBucket from "../storage/tracksBucket";
 import { Track } from "../db/schema";
 import { Kysely, sql, Transaction } from "kysely";
 import { Database } from "../db/schema";
+import * as Boom from "@hapi/boom";
 
 interface TrackOptions {
   name?: string;
@@ -26,6 +27,26 @@ export const getTrackBaseQuery = (dbTrx: Kysely<Database> | Transaction<Database
     ]);
 };
 
+const validateTrackNameIsUnique = async (name: string, excludeId?: number) => {
+  let query = db.selectFrom("tracks").selectAll().where("name", "=", name).where("deleted_at", "is", null);
+  if (excludeId) {
+    query = query.where("id", "!=", excludeId);
+  }
+  console.log("query: ", query.compile().sql);
+  console.log("parameters: ", query.compile().parameters);
+  const track = await query.executeTakeFirst();
+  console.log("track: ", track);
+  if (track) {
+    throw Boom.conflict("Track name is already taken");
+  }
+};
+
+const trackPermissionCheck = (track: Track, user_id: number) => {
+  if (track.user_id !== user_id) {
+    throw Boom.forbidden("You do not have permission to modify this playlist.");
+  }
+};
+
 export const getTracks = async (options: TrackOptions = {}) => {
   const { name, include_deleted = false } = options;
   let query = getTrackBaseQuery(db);
@@ -38,54 +59,66 @@ export const getTracks = async (options: TrackOptions = {}) => {
   return await query.execute();
 };
 
-export const getTrackById = async (id: number | string, trx?: Transaction<Database>) => {
+export const getTrackById = async (id: number, trx?: Transaction<Database>) => {
   const dbTrx = trx ?? db;
   let query = getTrackBaseQuery(dbTrx);
-  return await query.where("t.id", "=", Number(id)).executeTakeFirst();
+  const track = await query.where("t.id", "=", id).executeTakeFirst();
+  if (!track) {
+    throw Boom.notFound("Track not found");
+  }
+  return track;
 };
 
 export const getTrackFile = async (track: Track) => {
   const file = tracksBucket.file(`${track.id}.mp3`);
   const [exists] = await file.exists();
-  if (!exists) return null;
+  if (!exists) {
+    throw Boom.notFound("Track file not found");
+  }
   return file;
 };
 
-export const patchTrack = async (id: number | string, name: string) => {
-  await db.updateTable("tracks").set({ name }).where("id", "=", Number(id)).execute();
+export const patchTrack = async (id: number, name: string, user_id: number) => {
+  console.log("patchTrack");
+  const track = await getTrackById(id);
+  trackPermissionCheck(track, user_id);
+  await validateTrackNameIsUnique(name, id);
+  if (track.name === name) {
+    return track;
+  }
+  await db.updateTable("tracks").set({ name }).where("id", "=", id).execute();
   return await getTrackById(id);
 };
 
-export const deleteTrack = async (id: number | string) => {
+export const deleteTrack = async (id: number, user_id: number) => {
+  const track = await getTrackById(id);
+  trackPermissionCheck(track, user_id);
   await db
     .updateTable("tracks")
     .set({ deleted_at: new Date() })
-    .where("id", "=", Number(id))
+    .where("id", "=", id)
     .where("deleted_at", "is", null)
     .execute();
   return await getTrackById(id);
 };
 
-export const restoreTrack = async (id: number | string) => {
-  await db
-    .updateTable("tracks")
-    .set({ deleted_at: null })
-    .where("id", "=", Number(id))
-    .where("deleted_at", "is not", null)
-    .execute();
+export const restoreTrack = async (id: number, user_id: number, name?: string) => {
+  const track = await getTrackById(id);
+  trackPermissionCheck(track, user_id);
+  await patchTrack(id, name ?? track.name, user_id);
+  await db.updateTable("tracks").set({ deleted_at: null }).where("id", "=", id).execute();
   return await getTrackById(id);
 };
 
-export const postTrack = async (filepath: string, name: string, duration: number, user_id: number | string) => {
+export const postTrack = async (filepath: string, name: string, duration: number, user_id: number) => {
+  await validateTrackNameIsUnique(name);
   return await db.transaction().execute(async (trx) => {
+    // TODO: why is the insertId incrementing even on failed inserts?
     const { insertId } = await trx
       .insertInto("tracks")
-      .values({ name, duration, user_id: Number(user_id) })
+      .values({ name, duration, user_id: user_id })
       .executeTakeFirstOrThrow();
     const track = await getTrackById(Number(insertId), trx);
-    if (!track) {
-      throw new Error("Track not found after insert");
-    }
     await tracksBucket.upload(filepath, {
       destination: `${track.id}.mp3`,
       resumable: false,
