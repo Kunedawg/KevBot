2083da9f4f98c7015785fe3d1cf3294ff651d4a2
Kunedawg <Kevin.Kuney@gmail.com>
2024-11-22 07:57:27 -0600

    feat(api): Add testing setup
    - Installed new packages for testing: jest, ts-jest, supertest, and
      testcontainers.
    - Updated tsconfig to support testing. Added new base and test configs.
    - Setup jest for testing and added first test to tests directory.

---
+++ api/jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts", "**/tests/**/*.spec.ts"],
+  transform: {
+    "^.+.tsx?$": ["ts-jest", { tsconfig: "tsconfig.test.json" }],
+  },
+};
