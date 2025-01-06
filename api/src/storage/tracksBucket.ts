232d4dbb801f18cfc2957d69f547a3e350614235
Kunedawg <Kevin.Kuney@gmail.com>
2025-01-05 21:02:00 -0600

    fix

--- api/src/storage/tracksBucket.ts
+++ api/src/storage/tracksBucket.ts
@@ -2,6 +2,7 @@ import { Storage } from "@google-cloud/storage";
 
 export const tracksBucketFactory = (gcpApiEndpoint: string, tracksBucketName: string) => {
   const storage = new Storage({
+    projectId: "TODO-CHANGE-THIS",
     apiEndpoint: gcpApiEndpoint,
   });
   return storage.bucket(tracksBucketName);
