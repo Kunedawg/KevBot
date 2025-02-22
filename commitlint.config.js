2b32d3f92b0eae8d025ea7c3f966ce8e79029a0c
Kunedawg <Kevin.Kuney@gmail.com>
2025-02-22 15:44:00 -0600

    ci(project): update commit and pr linting
    - separated workflows into different files
    - commit linting runs on main pushes now
    - changes scope "repo" to "project"

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,6 @@ module.exports = {
   rules: {
     "header-max-length": [2, "always", 72],
     "body-max-line-length": [2, "always", 72],
-    "scope-enum": [2, "always", ["repo", "api", "frontend", "db", "gcloud", "tools", "bot"]],
+    "scope-enum": [2, "always", ["project", "api", "frontend", "db", "gcloud", "tools", "bot"]],
   },
 };
