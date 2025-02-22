aad8598c40957616c2f08c8936fa5959235304f0
Kunedawg <Kevin.Kuney@gmail.com>
2025-02-22 10:58:31 -0600

    test(api): add /docs tests
    - added simple test to make sure an html page is being returned
    - updated file names to be "docs" instead of "doc"

--- api/src/routes/docsRoutes.ts
+++ api/src/routes/docsRoutes.ts
@@ -2,7 +2,7 @@ import express from "express";
 import YAML from "yamljs";
 import swaggerUi from "swagger-ui-express";
 
-export function docRoutesFactory(swaggerYamlPath: string) {
+export function docsRoutesFactory(swaggerYamlPath: string) {
   const router = express.Router();
   const swaggerDocument = YAML.load(swaggerYamlPath);
   router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
