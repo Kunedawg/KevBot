bc85fbda86e9ae92bbbee4a76313332ac87a2769
Kunedawg <Kevin.Kuney@gmail.com>
2025-02-22 08:57:29 -0600

    feat: add /plays tests
    - added tests for /plays endpoint
    - removed the return message from /plays/tracks and
      /plays/playlists/random, it was not needed. 201 status code is enough.

--- api/src/schemas/playsSchemas.ts
+++ api/src/schemas/playsSchemas.ts
@@ -1,18 +1,22 @@
 import { z } from "zod";
 import { PLAY_TYPE } from "../services/playsService";
 
-export const logTracksPlayBodySchema = z.object({
-  track_id: z.number().int(),
-  play_type: z.union([
-    z.literal(PLAY_TYPE.PLAY),
-    z.literal(PLAY_TYPE.PLAY_RANDOM),
-    z.literal(PLAY_TYPE.GREETING),
-    z.literal(PLAY_TYPE.RAID),
-    z.literal(PLAY_TYPE.FAREWELL),
-    z.literal(PLAY_TYPE.CATEGORY_GREETING),
-  ]),
-});
+export const logTracksPlayBodySchema = z
+  .object({
+    track_id: z.number().int(),
+    play_type: z.union([
+      z.literal(PLAY_TYPE.PLAY),
+      z.literal(PLAY_TYPE.PLAY_RANDOM),
+      z.literal(PLAY_TYPE.GREETING),
+      z.literal(PLAY_TYPE.RAID),
+      z.literal(PLAY_TYPE.FAREWELL),
+      z.literal(PLAY_TYPE.CATEGORY_GREETING),
+    ]),
+  })
+  .strict();
 
-export const logRandomPlaylistsPlayBodySchema = z.object({
-  playlist_id: z.number().int(),
-});
+export const logRandomPlaylistsPlayBodySchema = z
+  .object({
+    playlist_id: z.number().int(),
+  })
+  .strict();
