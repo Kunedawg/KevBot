ac9fd5833e6f8c68bf035bb7a9266a21bac6c0e6
Kunedawg <Kevin.Kuney@gmail.com>
2025-10-01 17:13:06 -0500

    wip: search, switch to ngram parser

--- db/migration/migrations/2.11.0__add_track_search_indexes.sql
+++ db/migration/migrations/2.11.0__add_track_search_indexes.sql
@@ -1,7 +1,24 @@
--- Adds supporting indexes for track search and suggestion features.
+-- Adds supporting indexes and config for track search and suggestion features.
+
+-- check ngram token size is 2, fail if not
+DROP PROCEDURE IF EXISTS check_ngram;
+DELIMITER //
+
+CREATE PROCEDURE check_ngram()
+BEGIN
+  IF @@ngram_token_size != 2 THEN
+    SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'ngram_token_size must be 2';
+  END IF;
+END //
+
+DELIMITER ;
+
+CALL check_ngram();
+DROP PROCEDURE check_ngram;
 
 ALTER TABLE `tracks`
   ADD INDEX `idx_tracks_name` (`name`);
 
 ALTER TABLE `tracks`
-  ADD FULLTEXT INDEX `ftx_tracks_name` (`name`);
+  ADD FULLTEXT INDEX `ftx_tracks_name` (`name`) WITH PARSER ngram;
